9c6b283cf5606ba46e3df10a697b61c372a3e032
Sergey Svistun <svistunss.py@gmail.com>
2022-02-06 16:07:42 +0300

    fixes

---
+++ src/components/header/dropMenu.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+import { NavLink } from "react-router-dom";
+import classes from "@/components/header/header.module.scss";
+import IRoute from "@/types/iRoute";
+
+interface IDropMenuProps {
+  route: IRoute;
+}
+
+const DropMenu: FC<IDropMenuProps> = ({ route }) => (
+  <li key={route.url} className={classes.drop_menu}>
+    <NavLink
+      to={route.url}
+      className={({ isActive }) =>
+        isActive ? `${classes.link__active} ${classes.nav__routes_link}` : classes.nav__routes_link
+      }
+    >
+      {route.name}
+    </NavLink>
+    <ul className={classes.drop_menu__inner}>
+      {route.sub?.map((innerRoute) => (
+        <li className={classes.drop_menu__inner__item} key={innerRoute.id}>
+          <NavLink
+            to={innerRoute.url}
+            className={({ isActive }) =>
+              isActive
+                ? `${classes.drop_menu__inner_link__active} ${classes.drop_menu__inner_link}`
+                : classes.drop_menu__inner_link
+            }
+          >
+            {innerRoute.name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </li>
+);
+
+export default DropMenu;
