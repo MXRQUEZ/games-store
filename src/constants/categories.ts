3464e157e7dbd8b9893a17536c04fa111470811b
Sergey Svistun <svistunss.py@gmail.com>
2022-03-02 19:29:12 +0300

    bugfix

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,4 +1,3 @@
-import { v4 as getUniqueId } from "uuid";
 import ICategory from "@/types/iCategory";
 
 interface ICategories {
@@ -7,7 +6,7 @@ interface ICategories {
 
 export const categories: ICategories = {
   pc: {
-    id: getUniqueId(),
+    id: "pc",
     name: "PC",
     path: "pc",
     img: "https://media.discordapp.net/attachments/812670607950741554/939311033485500456/desktop-pc.png",
@@ -15,7 +14,7 @@ export const categories: ICategories = {
   },
 
   playstation: {
-    id: getUniqueId(),
+    id: "ps",
     name: "PlayStation 5",
     path: "playstation",
     img: "https://media.discordapp.net/attachments/812670607950741554/939311041685360681/Playstation.png",
@@ -23,7 +22,7 @@ export const categories: ICategories = {
   },
 
   xbox: {
-    id: getUniqueId(),
+    id: "xbox",
     name: "XBox One",
     path: "xbox",
     img: "https://cdn.discordapp.com/attachments/812670607950741554/939311023951843348/XBOX.png",
