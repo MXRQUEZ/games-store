a0e8054f68b9b46a8bf212e502d35abdeedb61d1
Sergey Svistun <svistunss.py@gmail.com>
2022-03-12 17:25:50 +0300

    js chunks

--- webpack.prod.js
+++ webpack.prod.js
@@ -43,6 +43,28 @@ module.exports = (env, argv) => {
         }),
         new OptimizeCSSAssetsPlugin({}), // it minifies css and optimize it with cssnano: https://cssnano.co/guides/optimisations
       ],
+      splitChunks: {
+        chunks: "async",
+        minSize: 30000,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        automaticNameDelimiter: "~",
+        automaticNameMaxLength: 30,
+        name: true,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
     },
     plugins: [
       // optional: it creates brotli-compressed files in '[path].gz[query]'
